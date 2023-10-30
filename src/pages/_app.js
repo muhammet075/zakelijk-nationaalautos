18fe9cd10df9d9dc917eee63cae8872986cea0e6
muhammet075 <muhammet075@icloud.com>
2023-10-30 16:18:33 +0100

    adding favico

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import favico from "../assets/favico/favico.png";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="format-detection" content="telephone=no"/>
         <meta name="viewport" content="width=device-width, user-scalable=no"/>
         <meta name="robots" content="noindex" />
+        <link href={favico.src} rel="icon" type="image/png"/>
       </Head>
       
       <div id='app'>
